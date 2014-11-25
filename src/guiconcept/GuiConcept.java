0ce924925c3782a9a2cb4ff076ad2d17d158b7bd
Eric Heidelmark <eheidelmark@gmail.com>
2014-11-24 20:58:43 -0500

    Initial commit for GUI

---
+++ src/guiconcept/GuiConcept.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiconcept;
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Eric
+ */
+public class GuiConcept {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
